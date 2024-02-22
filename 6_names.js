44d55c336bd41e4bb80163c7e6cc10fd6ba39206
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-02-22 08:53:24 +0500

    Delete 6_names.js

--- 6_names.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let name_WITHwhitespaces = '\n\tIRFAN MALIK \n';
-console.log(name_WITHwhitespaces);
-const name_WITHOUTwhitespaces = name_WITHwhitespaces.trim();
-console.log(name_WITHOUTwhitespaces);
