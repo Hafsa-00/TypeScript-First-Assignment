79a54ed970d16932a303a4c18a8022c297173d9f
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-02-22 08:55:30 +0500

    Delete 41_magicians.js

--- 41_magicians.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let magicians = ['hamdan', 'anus', 'naila', 'amna', 'mano'];
-function show_magicians(magicians) {
-    for (let magician of magicians) {
-        console.log(magician);
-    }
-}
-show_magicians(magicians);
