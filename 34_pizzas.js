ec46e77d7f646c1da22f4e935e8d8468bb2efd8e
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-02-22 08:59:31 +0500

    Delete 34_pizzas.js

--- 34_pizzas.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let pizzas = ['Pepperoni', 'Margherita', 'BBQ Chicken'];
-for (let pizza of pizzas) {
-    console.log(`I like ${pizza} pizza.`);
-}
-console.log("I love  eating  pizza! ");
