ec5055d5cfa65a3e0ac792bdbb732ae6a13528a3
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-02-22 08:58:51 +0500

    Delete 36_shirt.js

--- 36_shirt.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function make_shirt(size, message) {
-    console.log(`The size of the shirt is ${size} and the message printed on shirt is ${message}.`);
-}
-make_shirt('M', 'cutee');
