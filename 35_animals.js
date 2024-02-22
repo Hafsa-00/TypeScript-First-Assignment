324515053d46cf1dd7bb0e5e429c29328d7f773f
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-02-22 08:59:10 +0500

    Delete 35_animals.js

--- 35_animals.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let animals = ['sheep', 'cow', 'camel'];
-for (let animal of animals) {
-    console.log(animal);
-    console.log(`A ${animal} would make a great pet.`);
-}
-console.log('These animal are herbivores,domistic and mammal');
