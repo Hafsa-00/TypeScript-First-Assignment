b4d3f4854adc0e711180bbbb631090c7887d7f1f
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-02-22 08:52:37 +0500

    Delete 8_show_eight.js

--- 8_show_eight.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let add = (8 + 0);
-let minus = (8 - 0);
-let mutiply = (8 * 1);
-let divide = (8 / 1);
-console.log(`add: ${add},
-minus:${minus},
-mutiply:${mutiply},
-divide: ${divide}`);
