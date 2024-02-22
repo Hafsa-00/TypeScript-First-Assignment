f656f36ff8541f0bf1baf4f965d00ec41c1a894e
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-02-22 09:05:46 +0500

    Delete 19_invitation.js

--- 19_invitation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let invitees = ["maryam", "anusha", "ayesha"];
-invitees.forEach((invitee) => {
-    console.log(`Assalamualaikum ${invitee},\n I would like to invite you to dinner.\n Thank you.`);
-});
-console.log(`\n\nI am inviting ${invitees.length} people to dinner`);
