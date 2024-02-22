9daf02cdc6938709fcd1dd0a657d2ad84145dc56
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-02-22 08:56:23 +0500

    Delete 39_city.js

--- 39_city.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function city_country(city, country) {
-    console.log(`${city},${country}`);
-}
-city_country('hyderabad', 'pakistan');
-city_country('dehli', 'india');
-city_country('Sydney ', ' Australia');
